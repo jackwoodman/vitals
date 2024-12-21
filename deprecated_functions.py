cff0afda0c78f43468f46da3943d4d3ec6594681
Jack Woodman <jack.woodman@astrodigital.com>
2024-12-21 13:43:47 +1100

    Removed deprecated functions.

---
+++ deprecated_functions.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from classes import Measurement
+from metric_file_tools import add_measurement_to_metric_file, generate_metric_file, parse_health_metric
+
+
+def parse_metric_entry() -> Measurement:
+    print("\n -> Parsing new metric entry (metric_name value date):")
+    met_name, value_str, date_str = input("    ").split(" ")
+
+    return met_name, Measurement(
+        value=float(value_str), date=datetime.strptime(date_str, "%d/%m/%Y")
+    )
+
+
+def write():
+    direction = input("-> write new (metric) or new (entry)?\n")
+    if direction == "metric":
+        new_metric = parse_health_metric()
+        generate_metric_file(health_metric=new_metric)
+    else:
+        metric_name, new_entry = parse_metric_entry()
+
+        add_measurement_to_metric_file(metric_name=metric_name, measurement=new_entry)
+        print(f"\n === New entry for '{metric_name}' added === \n")
--- main.py
+++ main.py
@@ -2,38 +2,12 @@ from metric_file_tools import (
     FILE_DIR_NAME,
     create_metric_dir,
     load_metric_from_json,
-    parse_health_metric,
     read_metric_file_to_json,
     get_filenames_without_extension,
-    generate_metric_file,
-    add_measurement_to_metric_file,
     rename_health_file,
 )
 from data_entry import AssistedEntryHandler
 from logger import logger
-from datetime import datetime
-from classes import Measurement
-
-
-def parse_metric_entry() -> Measurement:
-    print("\n -> Parsing new metric entry (metric_name value date):")
-    met_name, value_str, date_str = input("    ").split(" ")
-
-    return met_name, Measurement(
-        value=float(value_str), date=datetime.strptime(date_str, "%d/%m/%Y")
-    )
-
-
-def write():
-    direction = input("-> write new (metric) or new (entry)?\n")
-    if direction == "metric":
-        new_metric = parse_health_metric()
-        generate_metric_file(health_metric=new_metric)
-    else:
-        metric_name, new_entry = parse_metric_entry()
-
-        add_measurement_to_metric_file(metric_name=metric_name, measurement=new_entry)
-        print(f"\n === New entry for '{metric_name}' added === \n")
 
 
 def new_write():
@@ -79,8 +53,6 @@ if __name__ == "__main__":
 
         if requirement == "read":
             read()
-        elif requirement == "write":
-            write()
         elif requirement == "rename":
             rename()
         elif requirement == "new_write":
